a345bc050b0b2b8894657be737c450b52919eba5
athanyuen <athany2025@student.cis.edu.hk>
2024-02-05 22:55:40 +0800

    Updated

--- app/src/main/java/com/example/newproject/ProfilePageActivity.java
+++ app/src/main/java/com/example/newproject/ProfilePageActivity.java
@@ -1,5 +1,6 @@
 package com.example.newproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,22 +9,50 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ProfilePageActivity extends AppCompatActivity {
     private Button signOutButton, backButton, rideHistoryButton;
-    private TextView phoneNumber;
+    private TextView nameTextView, uidTextView, usertypeTextView, ridestakenTextView, ownedvehiclesTextView, emailTextView;
     private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+        mAuth = FirebaseAuth.getInstance();
 
         signOutButton = this.findViewById(R.id.sign_out_button_profilePage);
         backButton = this.findViewById(R.id.back_button_profilePageActivity);
         rideHistoryButton = this.findViewById(R.id.ride_history_button);
+
+        nameTextView = this.findViewById(R.id.name_profilePage);
+        uidTextView = this.findViewById(R.id.uid_profilePage);
+        usertypeTextView = this.findViewById(R.id.usertype_profilePage);
+        ridestakenTextView = this.findViewById(R.id.ridesTaken_profilePage);
+        ownedvehiclesTextView = this.findViewById(R.id.ownedvehicles_profilePage);
+        emailTextView = this.findViewById(R.id.email_profilePage);
+
+        db = FirebaseFirestore.getInstance();
+        db.collection("users").document(mAuth.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot snapshot = task.getResult();
+                User currentUser = snapshot.toObject(User.class);
+                nameTextView.setText(String.valueOf(currentUser.getName()));
+                uidTextView.setText(String.valueOf(currentUser.getUid()));
+                usertypeTextView.setText(String.valueOf(currentUser.getUserType()));
+                emailTextView.setText(String.valueOf(currentUser.getEmail()));
+            }
+        });
+
 
 
 
@@ -52,4 +81,5 @@ public class ProfilePageActivity extends AppCompatActivity {
         });
 
     }
+
 }
